91169e0919b26e30443c40c76491a7c7c30a129a
Elizabeth Kipp <liz@elizabethkipp.com>
2020-03-03 15:07:39 -0600

    Updates

---
+++ precache-manifest.37794115dd514a785978c93953f59289.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8c12097d578c3b1539301fc25766ba",
+    "url": "/meetup-two/index.html"
+  },
+  {
+    "revision": "76d4d497ba4edc0b35b8",
+    "url": "/meetup-two/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3744b9195cbfc97688b5",
+    "url": "/meetup-two/static/css/main.6e8abd78.chunk.css"
+  },
+  {
+    "revision": "76d4d497ba4edc0b35b8",
+    "url": "/meetup-two/static/js/2.1cf04cec.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/meetup-two/static/js/2.1cf04cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3744b9195cbfc97688b5",
+    "url": "/meetup-two/static/js/main.098c3bd6.chunk.js"
+  },
+  {
+    "revision": "954e17cacaf12edffcca",
+    "url": "/meetup-two/static/js/runtime-main.06986970.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup-two/precache-manifest.9244c0ae3463c29f7938791ac9a22cbf.js"
+  "/meetup-two/precache-manifest.37794115dd514a785978c93953f59289.js"
 );
 
 self.addEventListener('message', (event) => {
