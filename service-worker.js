1fe8c5aac2568856f81bedb553abfcbc5914c023
Elizabeth Kipp <liz@elizabethkipp.com>
2020-03-06 17:36:48 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup-two/precache-manifest.e829401853daca12360978516e6f194c.js"
+  "/meetup-two/precache-manifest.b3b4fb4e78dd43f1755c09a4468ccd7f.js"
 );
 
 self.addEventListener('message', (event) => {
